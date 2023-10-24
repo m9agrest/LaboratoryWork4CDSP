4a888ab2611d76cbb209b285d2570d2ee6b12c4f
mina987 <erm.555.mmm@gmail.com>
2023-10-24 09:55:56 +0300

    Добавление класса Circle, который имеет в себе методы просмотра, находится ли точка внутри или на границе. и изменение Point, с структуры на класс

---
+++ Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWork4CDSP
+{
+    public class Circle : Point
+    {
+        public double R;
+        public Circle(double x, double y, double r) : base(x, y)
+        {
+            R = r;
+        }
+        public bool isInside(Point point) => R <= Distance(point);
+        public bool isBorder(Point point) => R == Distance(point);
+    }
+}
--- Point.cs
+++ Point.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace LaboratoryWork4CDSP
 {
-    public struct Point
+    public class Point
     {
         public double X;
         public double Y;
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
         public double Distance(Point point) => Math.Sqrt(Math.Pow(point.X - X, 2) + Math.Pow(point.Y - Y, 2));
     }
 }
