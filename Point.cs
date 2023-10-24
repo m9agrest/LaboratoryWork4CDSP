627c46f3f9ff9bd333236e25eba73ae241059517
mina987 <erm.555.mmm@gmail.com>
2023-10-24 09:46:05 +0300

    Добавленна структура Point, в которой реализован метод Distance, который в совю очередь считает расстояние до другой точки

---
+++ Point.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboratoryWork4CDSP
+{
+    public struct Point
+    {
+        public double X;
+        public double Y;
+        public double Distance(Point point) => Math.Sqrt(Math.Pow(point.X - X, 2) + Math.Pow(point.Y - Y, 2));
+    }
+}
